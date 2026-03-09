77a5e057fbc3686d02bfd04bab40a4503b1eab1c
Siddhant Kumar Singh <119556802+Siddhantsingh172@users.noreply.github.com>
2026-03-09 12:22:45 +0530

    Add files via upload

---
+++ DailyPracticeProblem/Sp[ring Project 3/Springproject3/src/in/cg/beans/Student.java
@@ -0,0 +1,34 @@
+package in.cg.beans;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+@Component
+public class Student {
+	@Value("1010")
+	private int id;
+	@Value("Sidd")
+	private String name;
+	@Value("sidd@lpu.in")
+	private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void display() {
+		System.out.println("ID: "+id+ "  Name: "+name+"Email: "+email);
+	}
+}
